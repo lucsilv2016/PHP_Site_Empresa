d43205640336fb4cfec236326c2c156b821ef3fe
luciano22desouza <luciano22desouza@gmail.com>
2020-07-22 23:20:36 +0000

    Criacao de arquivos - cabecalho, rodape e topo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<? 
-  
- /*Projeto Site Empresa PHP*/
+<?php include('html_header.php');?>
+<?php include('topo.php');?>
 
-?>
+<h1>Página inicial</h1>
+
+<?php include('html_footer.php');?>
---
+++ topo.php
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-6">[logo da empresa]</div>
+        <div class="col-6 text-right">[links de navegacao]</div>
+    </div>
+</div>
